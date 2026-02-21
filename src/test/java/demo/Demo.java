8a0ef8c7e557951cbbf3bd5a478dfaa5230f617e
Sumit Kumar <sumit2@quadrafort.com>
2026-02-21 17:42:19 +0530

    first commit

---
+++ src/test/java/demo/Demo.java
@@ -0,0 +1,36 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+
+	WebDriver driver = null;
+
+	@BeforeMethod
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo");
+	}
+
+	@AfterMethod
+	public void testdown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void checkTitle() {
+		Assert.assertEquals(driver.getTitle(), "Your Store");
+	}
+
+}
